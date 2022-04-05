f2f00d26555415b84ab1daebe008b760b484028c
Emilie LSM <99273880+emilie06lsm@users.noreply.github.com>
2022-04-05 22:03:41 +0000

    last edit

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="description" content="My first webpage with user input">
     <meta name="keywords" content="immaculata, ics2o">
-    <meta name="author" content="Ms Raffin">
+    <meta name="author" content="Emilie">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Code for the favicon -->
     <link rel="apple-touch-icon" sizes="180x180" href="./favicon/apple-touch-icon.png">
